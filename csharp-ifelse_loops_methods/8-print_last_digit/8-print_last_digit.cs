6cdb905159307d2d400ae3b62934825593e01d4b
Pierre-Dureau <dureaupierre53@gmail.com>
2023-07-25 17:04:43 +0200

    Print last digit

---
+++ csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        number = number % 10;
+        if (number < 0)
+            number = number * -1;
+        Console.Write(number);
+        return number;
+    }
+}
