d0df5ae803338fb066ae8ee20c8c70dca6a5776c
Pierre-Dureau <dureaupierre53@gmail.com>
2023-07-25 16:50:18 +0200

    Print numbers without repetitions

---
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 80; i++)
+            {
+                if (i / 10 < i % 10)
+                    Console.Write($"{i:D2}, ");
+            }
+            Console.WriteLine("89");
+        }
+    }
+}
