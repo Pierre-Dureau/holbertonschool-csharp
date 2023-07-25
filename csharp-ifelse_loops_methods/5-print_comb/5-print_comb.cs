8362e1aeee69fb0b65b7bef8490280575fe95a79
Pierre-Dureau <dureaupierre53@gmail.com>
2023-07-25 16:47:06 +0200

    00...99

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 99; i++)
+            {
+                Console.Write($"{i:D2}, ");
+            }
+            Console.Write("99\n");
+        }
+    }
+}
