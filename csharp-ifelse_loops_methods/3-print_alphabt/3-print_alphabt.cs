5fd1ca6cedb89f993c03aadc68aa48649f7a6bdd
Pierre-Dureau <dureaupierre53@gmail.com>
2023-07-25 16:32:52 +0200

    Print alphabt

---
+++ csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _3_print_alphabt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char let = 'a';
+
+            for (; let <= 'z'; let++)
+            {
+                if (let != 'e' && let != 'q')
+                    Console.Write(let);
+            }
+        }
+    }
+}
