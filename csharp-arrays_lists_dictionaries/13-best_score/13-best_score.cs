e8f6743e25fe7da6bb163bf81a2dc079ac1ff37c
Pierre-Dureau <dureaupierre53@gmail.com>
2023-07-31 13:34:19 +0200

    Best score

---
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        string name = "None";
+        int max = 0;
+
+        if (myList.Count == 0)
+            return name;
+
+        foreach (var item in myList)
+        {
+            if (item.Value > max)
+            {
+                max = item.Value;
+                name = item.Key;
+            }
+        }
+
+        return name;
+    }
+}
