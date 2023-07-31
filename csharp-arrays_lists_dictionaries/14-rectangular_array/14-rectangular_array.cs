08520daac4dd5ccbe3ae7713f6e449954ff28373
Pierre-Dureau <dureaupierre53@gmail.com>
2023-07-31 13:46:27 +0200

    Rectangular array

---
+++ csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int [,] array = new int[5,5] {
+            {0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0},
+            {0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0}
+        };
+
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                if (j != 4)
+                    Console.Write($"{array[i,j]} ");
+                else
+                    Console.Write($"{array[i,j]}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
