a507d199fba439f00b03918360f510601473a8f0
Pierre-Dureau <dureaupierre53@gmail.com>
2023-07-31 14:06:44 +0200

    Jagged array

---
+++ csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[][] jaggedArray = new int[3][];
+
+        jaggedArray[0] = new int[] { 0, 1, 2, 3 };
+        jaggedArray[1] = new int[] { 0, 1, 2, 3, 4, 5, 6 };
+        jaggedArray[2] = new int[] { 0, 1 };
+
+        for (int i = 0; i < jaggedArray.GetLength(0); i++)
+        {
+            for (int j = 0; j < jaggedArray[i].Length; j++)
+            {
+                if (j != 0)
+                    Console.Write(" ");
+                Console.Write($"{jaggedArray[i][j]}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
