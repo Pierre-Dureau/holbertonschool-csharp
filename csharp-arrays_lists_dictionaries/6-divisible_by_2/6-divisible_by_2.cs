9a5c669dc509b3cf35ce14199937e90dd941cf09
Pierre-Dureau <dureaupierre53@gmail.com>
2023-07-31 12:51:25 +0200

    Only by 2

---
+++ csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        List<bool> result = new List<bool>();
+
+        foreach (var item in myList)
+        {
+            result.Add(item % 2 == 0 ? true : false);
+        }
+        return result;
+    }
+}
