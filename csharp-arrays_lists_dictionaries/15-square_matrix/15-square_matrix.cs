6f8b916058b9f121f864aced981600369d78028f
Pierre-Dureau <dureaupierre53@gmail.com>
2023-07-31 13:58:36 +0200

    Matrix squared

--- csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
+++ csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -4,11 +4,11 @@ class Matrix
 {
     public static int[,] Square(int[,] myMatrix)
     {
-        int [,] newMatrix = new int[myMatrix.GetLength(0),myMatrix.GetLength(0)];
+        int [,] newMatrix = new int[myMatrix.GetLength(0),myMatrix.GetLength(1)];
 
         for (int i = 0; i < myMatrix.GetLength(0); i++)
         {
-            for (int j = 0; j < myMatrix.GetLength(0); j++)
+            for (int j = 0; j < myMatrix.GetLength(1); j++)
             {
                 newMatrix[i,j] = myMatrix[i,j] * myMatrix[i,j];
             }
