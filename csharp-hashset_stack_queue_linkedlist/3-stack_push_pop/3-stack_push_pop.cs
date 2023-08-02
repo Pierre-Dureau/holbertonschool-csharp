683dff87cc319bfe81afc108495a9c2ef47c70b3
Pierre-Dureau <dureaupierre53@gmail.com>
2023-08-02 10:43:45 +0200

    Full stack

---
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        int n = aStack.Count;
+
+        Console.WriteLine($"Number of items: {n}");
+
+        if (n > 0)
+            Console.WriteLine($"Top item: {aStack.Peek()}");
+        else
+            Console.WriteLine("Stack is empty");
+
+        if (!aStack.Contains(search))
+            Console.WriteLine($"Stack contains \"{search}\": False");
+        else
+        {
+            Console.WriteLine($"Stack contains \"{search}\": True");
+            while (aStack.Pop() != search)
+                continue;
+            aStack.Push(newItem);
+        }
+
+        return aStack;
+    }
+}
