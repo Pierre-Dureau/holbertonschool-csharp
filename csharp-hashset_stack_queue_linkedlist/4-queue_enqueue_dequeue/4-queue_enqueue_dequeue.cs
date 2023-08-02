b1fa76edb11a1da3a7cc99fdbd014161061b5448
Pierre-Dureau <dureaupierre53@gmail.com>
2023-08-02 10:53:08 +0200

    Queue it up

---
+++ csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        int n = aQueue.Count;
+
+        Console.WriteLine($"Number of items: {n}");
+
+        if (n > 0)
+            Console.WriteLine($"First item: {aQueue.Peek()}");
+        else
+            Console.WriteLine("Queue is empty");
+
+        if (!aQueue.Contains(search))
+            Console.WriteLine($"Queue contains \"{search}\": False");
+        else
+        {
+            Console.WriteLine($"Queue contains \"{search}\": True");
+            while (aQueue.Dequeue() != search)
+                continue;
+        }
+        
+        aQueue.Enqueue(newItem);
+
+        return aQueue;
+    }
+}
