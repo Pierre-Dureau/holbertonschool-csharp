e43e9cfaf1e35effc39d8b75300fc6c67260a2ac
Pierre-Dureau <dureaupierre53@gmail.com>
2023-08-02 09:43:52 +0200

    Present in both

---
+++ csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        List<int> newList = new List<int>();
+        int check = 0;
+
+        foreach (var item in list1)
+        {
+            foreach (var item2 in list2)
+            {
+                if (item == item2)
+                {
+                    newList.Add(item);
+                    break;
+                }
+            }
+        }
+
+        return newList;
+    }
+}
