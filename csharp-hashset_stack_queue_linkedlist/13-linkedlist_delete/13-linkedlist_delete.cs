7470d73d630266d1b8f7fd11e39ba1f140747241
Pierre-Dureau <dureaupierre53@gmail.com>
2023-08-02 11:57:26 +0200

    Delete at index

---
+++ csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        int i = 0;
+
+        foreach (var item in myLList)
+        {
+            if (i == index)
+            {
+                myLList.Remove(item);
+                break;
+            }
+            i++;
+        }
+    }
+}
