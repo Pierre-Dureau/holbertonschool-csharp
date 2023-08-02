8983e68b499316870ad39cbfdecbe6c0b1d46e9e
Pierre-Dureau <dureaupierre53@gmail.com>
2023-08-02 09:36:47 +0200

    Unique addition

---
+++ csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        int sum = 0;
+        int check = 0;
+        
+        for (int i = 0; i < myList.Count; i++)
+        {
+            for (int j = i + 1; j < myList.Count; j++)
+            {
+                if (myList[i] == myList[j])
+                    check = 1;
+            }
+
+            if (check == 0)
+                sum += myList[i];
+            else
+                check = 0;
+        }
+
+        return sum;
+    }
+}
