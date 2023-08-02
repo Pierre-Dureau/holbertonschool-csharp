4202bf97a437b9620fcc0aa46e06f4be3a2c1c25
Pierre-Dureau <dureaupierre53@gmail.com>
2023-08-02 11:15:38 +0200

    Pop

---
+++ csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Pop(LinkedList<int> myLList)
+    {
+        int first = myLList.First.Value;
+
+        myLList.RemoveFirst();
+
+        return first;
+    }
+}
