dfc519c88ac3bf398c37adf2935a8188b4997883
Dureau <dureaupierre53@gmail.com>
2023-08-21 11:13:09 +0200

    2nd Constructor

---
+++ csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Represent Zombie class
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// Represent the health of the zombie
+        /// </summary>
+        public int health;
+
+        /// <summary>
+        /// Constructor of a Zombie
+        /// </summary>
+        public Zombie() {
+            health = 0;
+        }
+
+        /// <summary>
+        /// Zombie Constructor with parameter
+        /// </summary>
+        /// <param name="value"> Health </param>
+        /// <exception cref="ArgumentException"> If health inferior 0 </exception>
+        public Zombie(int value) {
+            if (value < 0)
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            else
+                health = value;
+        }
+    }
+}
