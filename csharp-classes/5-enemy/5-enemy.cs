a044a02901237188f572a92827c6a1233d4b5a36
Dureau <dureaupierre53@gmail.com>
2023-08-21 12:09:04 +0200

    ToString

---
+++ csharp-classes/5-enemy/5-enemy.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Represent Zombie class
+    /// </summary>
+    public class Zombie
+    {
+        private int health;
+        private string name = "(No name)";
+
+        /// <summary>
+        /// Constructor of a Zombie
+        /// </summary>
+        public Zombie() {
+            health = 0;
+        }
+
+        /// <summary>
+        /// Zombie Constructor with parameter
+        /// </summary>
+        /// <param name="value"> Health </param>
+        /// <exception cref="ArgumentException"> If health inferior 0 </exception>
+        public Zombie(int value) {
+            if (value < 0)
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            else
+                health = value;
+        }
+
+        /// <summary>
+        /// Health accessor
+        /// </summary>
+        /// <returns> Health </returns>
+        public int GetHealth() {
+            return this.health;
+        }
+
+        /// <summary>
+        /// Accessor of name
+        /// </summary>
+        public string Name { get => name; set => name = value; }
+
+        /// <summary>
+        /// override the ToString function
+        /// </summary>
+        /// <returns> The new string </returns>
+        public override string ToString() => $"Zombie Name: {this.Name} / Total Health: {this.GetHealth()}";
+    }
+}
