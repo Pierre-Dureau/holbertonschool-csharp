61386aa244962c16b34482b527eebfab667cbcbb
Dureau <dureaupierre53@gmail.com>
2023-08-21 10:53:28 +0200

    Constructor

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,20 @@
+﻿namespace Enemies
+{
+    /// <summary>
+    /// Represent Zombie class
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// Represent the health of the zombie
+        /// </summary>
+        public int health;
+
+        /// <summary>
+        /// Constructor of a Zombie
+        /// </summary>
+        public Zombie() {
+            health = 0;
+        }
+    }
+}
