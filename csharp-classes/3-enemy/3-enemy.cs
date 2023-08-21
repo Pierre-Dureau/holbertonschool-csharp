9815e12a4cda4591c0c741311955c3b3df4262f0
Dureau <dureaupierre53@gmail.com>
2023-08-21 11:17:33 +0200

    Accessor

---
+++ csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Represent Zombie class
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// Represent the health of the zombie
+        /// </summary>
+        private int health;
+
+        /// <summary>
+        /// Constructor of a Zombie
+        /// </summary>
+        public Zombie() {
+            health = 0;
+        }
+
+        /// <summary>
+        /// Zombie Constructor with parameter
+        /// </summary>
+        /// <param name="value"> Health </param>
+        /// <exception cref="ArgumentException"> If health inferior 0 </exception>
+        public Zombie(int value) {
+            if (value < 0)
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            else
+                health = value;
+        }
+
+        /// <summary>
+        /// Health accessor
+        /// </summary>
+        /// <returns> Health </returns>
+        public int GetHealth() {
+            return this.health;
+        }
+    }
+}
