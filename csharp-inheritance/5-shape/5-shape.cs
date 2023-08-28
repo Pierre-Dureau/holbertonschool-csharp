e957af0dfe1aa03c3be01c764f235311fc188b90
Dureau <dureaupierre53@gmail.com>
2023-08-28 11:03:42 +0200

    Basic Shapes

---
+++ csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/// <summary>
+/// Shape class
+/// </summary>
+public class Shape
+{
+    /// <summary>
+    /// Not implemented
+    /// </summary>
+    /// <returns> Not implemented </returns>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
