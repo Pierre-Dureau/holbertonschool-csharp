b126302680dcd0c939e671835ed60d51af4ba543
Dureau <dureaupierre53@gmail.com>
2023-08-28 07:06:00 +0200

    Type of Array

---
+++ csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// Obj class
+/// </summary>
+class Obj
+{
+    /// <summary>
+    /// Returns True if the object is an instance of, or if the object is an instance of a class that inherited from, Array
+    /// </summary>
+    /// <param name="obj"> The obj </param>
+    /// <returns> True or False </returns>
+    public static bool IsInstanceOfArray(object obj)
+    {
+        // obj is Int32[]
+        // obj.GetType() == typeof(Int32[])
+        return (obj.GetType().IsArray);
+    }
+}
