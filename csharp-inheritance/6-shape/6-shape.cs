f11f9bc520e204ab4be7c6c882382ce143238cae
Dureau <dureaupierre53@gmail.com>
2023-08-28 11:22:30 +0200

    Rectangle

---
+++ csharp-inheritance/6-shape/6-shape.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+/// <summary>
+/// Shape class
+/// </summary>
+public class Shape
+{
+    /// <summary>
+    /// Not implemented
+    /// </summary>
+    /// <returns> Not implemented </returns>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+/// <summary>
+/// Rectangle class
+/// </summary>
+public class Rectangle : Shape
+{
+    private int width;
+    private int height;
+
+    /// <summary>
+    /// Get / Set the width
+    /// </summary>
+    public int Width 
+    {
+        get => width;
+        set {
+            if (value < 0)
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            else
+                width = value;
+        }
+    }
+
+    /// <summary>
+    /// Get / Set the height
+    /// </summary>
+    public int Height
+    {
+        get => height;
+        set {
+            if (value < 0)
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            else
+                height = value;
+        }
+    }
+}
