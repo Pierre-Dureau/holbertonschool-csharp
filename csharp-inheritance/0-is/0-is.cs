e0a806c0c09d290d97b3b548b8e6698d0f477f52
Dureau <dureaupierre53@gmail.com>
2023-08-28 07:03:51 +0200

    Type of Int

--- csharp-inheritance/0-is/0-is.cs
+++ csharp-inheritance/0-is/0-is.cs
@@ -1,7 +1,15 @@
 ﻿using System;
 
+/// <summary>
+/// Obj class
+/// </summary>
 class Obj
 {
+    /// <summary>
+    /// returns True if the object is an int
+    /// </summary>
+    /// <param name="obj"> The obj </param>
+    /// <returns> True or False </returns>
     public static bool IsOfTypeInt(object obj)
     {
         return (obj is int);
