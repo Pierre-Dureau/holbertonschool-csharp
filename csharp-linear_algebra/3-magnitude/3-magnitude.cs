8772e394a3859202fb8adb9ce2ee20f5f2da9ec9
Dureau <dureaupierre53@gmail.com>
2023-09-11 11:38:15 +0200

    add tasks

---
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+/// <summary>
+/// class VectorMath
+/// </summary>
+class VectorMath
+{
+    /// <summary>
+    /// Calculates the length of a given vector
+    /// </summary>
+    /// <param name="vector"> Array of double </param>
+    /// <returns> The length or -1 if not a vector 2D or 3D </returns>
+    public static double Magnitude(double[] vector)
+    {
+        int length = vector.Length;
+        double n = 0;
+
+        if (length != 2 && length != 3)
+            return -1;
+
+        foreach (var item in vector)
+        {
+            n += item * item;
+        }
+
+        return (Math.Round(Math.Sqrt(n), 2));
+    }
+}
---
+++ csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+class VectorMath
+{
+    public static double[] Add(double[] vector1, double[] vector2)
+    {
+        int len1 = vector1.Length;
+        int len2 = vector2.Length;
+
+        if ((len1 != 2 && len1 != 3) || (len1 != len2))
+        {
+            return -1;
+        }
+
+        double[] resultVector = new double[len1];
+
+        for (int i = 0; i < len1; i++)
+        {
+            resultVector[i] = vector1[i] + vector2[i];
+        }
+
+        return resultVector;
+    }
+}
