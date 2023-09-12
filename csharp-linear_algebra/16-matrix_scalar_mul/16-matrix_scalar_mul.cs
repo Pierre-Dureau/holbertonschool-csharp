e6a3dcf66b65a00475e1a0d4918ebd900187797b
Dureau <dureaupierre53@gmail.com>
2023-09-12 11:25:38 +0200

    add tasks

--- csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
+++ csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -15,7 +15,7 @@ class MatrixMath
     {
         if (matrix.GetLength(0) != matrix.GetLength(1))
             return new double[,] { { -1 } };
-        if (matrix.GetLength(0) != 2 || matrix.GetLength(0) != 3)
+        if (matrix.GetLength(0) != 2 && matrix.GetLength(0) != 3)
             return new double[,] { { -1 } };
 
         double[,] result = new double[matrix.GetLength(0), matrix.GetLength(1)];
