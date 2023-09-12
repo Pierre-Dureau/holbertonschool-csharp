b957d52f6fa900f1ccf6849a4f1f7ae054e3210e
Dureau <dureaupierre53@gmail.com>
2023-09-12 14:12:27 +0200

    add tasks

--- csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
+++ csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -13,18 +13,19 @@ class MatrixMath
     /// <returns> The resulting matrix or -1 </returns>
     public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
     {
-        if (matrix1.GetLength(0) != matrix2.GetLength(1))
+        if (matrix1.GetLength(1) != matrix2.GetLength(0))
             return new double[,] { { -1 } };
 
-        double[,] result = new double[matrix1.GetLength(1), matrix2.GetLength(1)];
+        double[,] result = new double[matrix1.GetLength(0), matrix2.GetLength(1)];
 
-        for (int i = 0; i < matrix2.GetLength(0); i++)
+        for (int i = 0; i < matrix2.GetLength(1); i++)
         {
-            for (int j = 0; j < matrix1.GetLength(1); j++)
+            for (int j = 0; j < matrix1.GetLength(0); j++)
             {
-                for (int k = 0; k < matrix1.GetLength(0); k++)
+                result[j,i] = 0;
+                for (int k = 0; k < matrix1.GetLength(1); k++)
                 {
-                    result[j,i] = matrix1[j,k] * matrix2[k,i];
+                    result[j,i] += matrix1[j,k] * matrix2[k,i];
                 }
             }
         }
