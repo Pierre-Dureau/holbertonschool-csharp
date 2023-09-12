da633346d6ba207bd143a166a7a9493fd105f61e
Dureau <dureaupierre53@gmail.com>
2023-09-12 10:30:37 +0200

    add tasks

--- csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -27,7 +27,7 @@ class MatrixMath
 
         for (int i = 0; i < matrix1.GetLength(0); i++)
         {
-            for (int j = 0; i < matrix1.GetLength(1); j++)
+            for (int j = 0; j < matrix1.GetLength(1); j++)
             {
                 result[i,j] = matrix1[i,j] + matrix2[i,j];
             }
