63439ecc604ffe59f8076892201754c18e1c71ca
Dureau <dureaupierre53@gmail.com>
2023-09-12 10:18:59 +0200

    add tasks

--- csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -20,7 +20,7 @@ class MatrixMath
         if (matrix1.GetLength(0) != matrix2.GetLength(0))
             return new double[,] { -1 };
         // Check size 2 or 3
-        if (matrix1.GetLength(0) != 2 && matrix1.GetLength(0) != 3))
+        if (matrix1.GetLength(0) != 2 && matrix1.GetLength(0) != 3)
             return new double[,] { -1 };
 
         double[,] result = new double[matrix1.GetLength(0), matrix1.GetLength(1)];
