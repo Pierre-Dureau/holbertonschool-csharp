b033eb1c4213c1036877f99bcb57e32752c1aea3
Dureau <dureaupierre53@gmail.com>
2023-09-14 09:46:19 +0200

    add tasks

---
+++ csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+/// <summary>
+/// class MatrixMath
+/// </summary>
+class MatrixMath
+{
+    /// <summary>
+    /// Transpose a matrix
+    /// </summary>
+    /// <param name="matrix"> The matrix </param>
+    /// <returns> The resulting matrix </returns>
+    public static double[,] Transpose(double[,] matrix)
+    {
+        double[,] transposedMatrix = new double[matrix.GetLength(1), matrix.GetLength(0)];
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                transposedMatrix[j, i] = matrix[i, j];
+            }
+        }
+
+        return transposedMatrix;
+    }
+}
+
