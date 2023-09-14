1c11df06dbf60a91c7b350f3364f7d60630779ee
Dureau <dureaupierre53@gmail.com>
2023-09-14 11:44:54 +0200

    add tasks

--- csharp-linear_algebra/30-cross_product/30-cross_product.cs
+++ csharp-linear_algebra/30-cross_product/30-cross_product.cs
@@ -13,13 +13,13 @@ class VectorMath
     /// <returns> The resulting vector or -1 </returns>
     public static double[] CrossProduct(double[] vector1, double[] vector2)
     {
-        if ((vector1.GetLength(1) != 3 || vector1.GetLength(0) != 1) || (vector2.GetLength(1) != 3 || vector2.GetLength(0) != 1))
-            return new double[1, 1] { { -1 } };
+        if (vector1.Length != 3 || vector2.Length != 3)
+            return new double[1] { -1 };
 
-        double cX = Math.Round(vector1[0, 1] * vector2[0, 2] - vector1[0, 2] * vector2[0, 1], 2);
-        double cY = Math.Round(vector1[0, 2] * vector2[0, 0] - vector1[0, 0] * vector2[0, 2], 2);
-        double cZ = Math.Round(vector1[0, 0] * vector2[0, 1] - vector1[0, 1] * vector2[0, 0], 2);
+        double cX = Math.Round(vector1[1] * vector2[2] - vector1[2] * vector2[1], 2);
+        double cY = Math.Round(vector1[2] * vector2[0] - vector1[0] * vector2[2], 2);
+        double cZ = Math.Round(vector1[0] * vector2[1] - vector1[1] * vector2[0], 2);
 
-        return new double[,] {{cX, cY, cZ}};
+        return new double[3] {cX, cY, cZ};
     }
 }
