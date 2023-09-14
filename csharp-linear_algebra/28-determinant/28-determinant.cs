f6c4a96c9151160e67b695d1ab9f76f2d8f5a6c6
Dureau <dureaupierre53@gmail.com>
2023-09-14 10:06:54 +0200

    add tasks

--- csharp-linear_algebra/28-determinant/28-determinant.cs
+++ csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -14,17 +14,17 @@ class MatrixMath
     {
         // Check square matrix
         if (matrix.GetLength(0) != matrix.GetLength(1))
-            return new double[,] {{-1}};
+            return -1;
         // Check size 2 or 3
         if (matrix.GetLength(0) != 2 && matrix.GetLength(0) != 3)
-            return new double[,] {{-1}};
+            return -1;
 
         if (matrix.GetLength(0) == 2)
-            return matrix[0, 0] * matrix[1, 1] + matrix[0, 1] * matrix[1, 0];
+            return matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0];
 
-        double first = matrix[0, 0] * (matrix[1, 1] * matrix[2, 2] + matrix[1, 2] * matrix[2, 1]);
-        double secnd = matrix[0, 1] * (matrix[1, 0] * matrix[2, 2] + matrix[1, 2] * matrix[2, 0]);
-        double third = matrix[0, 2] * (matrix[1, 0] * matrix[2, 1] + matrix[1, 1] * matrix[2, 0]);
+        double first = matrix[0, 0] * (matrix[1, 1] * matrix[2, 2] - matrix[1, 2] * matrix[2, 1]);
+        double secnd = matrix[0, 1] * (matrix[1, 0] * matrix[2, 2] - matrix[1, 2] * matrix[2, 0]);
+        double third = matrix[0, 2] * (matrix[1, 0] * matrix[2, 1] - matrix[1, 1] * matrix[2, 0]);
 
         return first - secnd + third;
     }
