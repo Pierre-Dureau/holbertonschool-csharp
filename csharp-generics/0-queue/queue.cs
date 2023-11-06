c20a349fc22909811b8912f87fc9bc2a51bfc667
Pierre-Dureau <dureaupierre53@gmail.com>
2023-11-06 09:21:53 +0100

    check type

---
+++ csharp-generics/0-queue/queue.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// Represents a generic queue data structure.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class Queue<T>
+{
+    /// <summary>
+    /// Returns the type of the element
+    /// </summary>
+    /// <returns> The type </returns>
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+}
